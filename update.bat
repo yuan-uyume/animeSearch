ee688d43b8af2b950ecd87837b9633adcafc2663
uyume <342877212@qq.com>
2022-05-30 13:58:44 +0800

    更新功能

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 set urlbase=https://raw.fastgit.org/dakerj/animeSearch/master/
 set download=aria2\aria2c
 echo 获取更新内容
-%download% "%urlbase%updateValue.bat"
 rm updateValue.bat
+%download% "%urlbase%updateValue.bat"
 call updateValue.bat
