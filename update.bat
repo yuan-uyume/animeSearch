a50ba93dad36e056debbc6c2de86cdd79d01f1ab
uyume <342877212@qq.com>
2022-06-10 23:24:45 +0800

    0.152 优化在线更新提示 尝试解决windows脚本换行符问题

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 set urlbase=https://raw.fastgit.org/dakerj/animeSearch/master/
 set download=aria2\aria2c
-echo 获取更新内容
+echo 正在获取更新内容...
 rm updateValue.bat
 %download% "%urlbase%updateValue.bat"
 call updateValue.bat
--- updateValue.bat
+++ updateValue.bat
@@ -1,6 +1,6 @@
 set urlbase=https://raw.fastgit.org/dakerj/animeSearch/master/
 set download=aria2\aria2c
-echo 开始删除旧文件
+echo 开始删除旧文件...
 rm html\index.html
 rm html\version.json
 rm html\js\uyume-parse.js
@@ -11,5 +11,5 @@ echo 开始下载
 %download% -d html\js "%urlbase%html/js/uyume-parse.js"
 %download% -d html\css "%urlbase%html/css/my.css"
 %download% -d openresty\conf "%urlbase%openresty/conf/nginx.conf"
-echo 下载结束,重载配置
+echo 下载结束...重载配置
 openresty\nginx -s reload -c openresty\conf\nginx.conf
