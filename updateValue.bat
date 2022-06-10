cd9d05f84eff9272a4c440b706fcf2b0c42ab8ed
uyume <342877212@qq.com>
2022-06-11 00:37:21 +0800

    0.153 在线更新问题测试2

--- updateValue.bat
+++ updateValue.bat
@@ -4,7 +4,7 @@ echo 开始删除旧文件...
 del /f html\rem\.*?
 del /f openresty\conf\nginx.conf
 echo 开始下载
-%download% -d html\rem -Z "%urlbase%html/index.html" "%urlbase%html/version.json" "%urlbase%html/js/uyume-parse.js" "%urlbase%html/css/my.css"
+%download% -d html\rem -Z "%urlbase%html/rem/index.html" "%urlbase%html/rem/version.json" "%urlbase%html/rem/uyume-parse.js" "%urlbase%html/rem/my.css"
 %download% -d openresty\conf "%urlbase%openresty/conf/nginx.conf"
 echo 下载结束...重载配置
 openresty\nginx -s reload -c openresty\conf\nginx.conf
