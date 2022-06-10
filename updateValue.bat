723514caa47f2e5af2d9632ff8583a5169027905
uyume <342877212@qq.com>
2022-06-11 00:51:08 +0800

    0.153 在线更新问题测试3

--- updateValue.bat
+++ updateValue.bat
@@ -1,7 +1,7 @@
 set urlbase=https://raw.fastgit.org/dakerj/animeSearch/master/
 set download=aria2\aria2c
 echo 开始删除旧文件...
-del /f html\rem\.*?
+del /f/q  html\rem\*.*
 del /f openresty\conf\nginx.conf
 echo 开始下载
 %download% -d html\rem -Z "%urlbase%html/rem/index.html" "%urlbase%html/rem/version.json" "%urlbase%html/rem/uyume-parse.js" "%urlbase%html/rem/my.css"
